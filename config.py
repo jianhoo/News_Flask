bada0fb2404b660ecd4399bb78fa050925ffd298
eric <278810505@qq.com>
2018-09-27 16:37:45 +0800

    1.添加mysql,redis,session等配置

---
+++ config.py
@@ -0,0 +1,19 @@
+import redis
+
+
+class Config(object):
+    """项目配置信息"""
+    DEBUG = True
+    # 配置连接的数据库
+    SQLALCHEMY_DATABASE_URI = "mysql+dbmysql://root:A92b11c20@127.0.0.1:3306/information"
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    # 配置redis数据库
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+
+    # flask__session的配置信息
+    SECRET_KEY = "1234567890"
+    SESSION_TYPE = "redis"  # 指定session保存到redis中
+    SESSION_USE_SIGNER = True  # 让cookie中的seesion_id 被加密签名处理
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    PERMANENT_SESSION_LIFETIME = 86400  # session 的有效期,单位是秒
--- manage.py
+++ manage.py
@@ -1,6 +1,27 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_session import Session
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+import redis
+from config import Config
 
 app = Flask(__name__)
+
+
+# 设置app应用的config配置
+app.config.from_object(Config)
+# 构建数据库对象
+db = SQLAlchemy(app)
+# 构建redis数据库对象
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+Session(app)
+
+# 创建管理对象
+manager = Manager(app)
+Migrate(app, db)
+# 添加db命令
+manager.add_command('db', MigrateCommand)
 
 
 @app.route('/')
