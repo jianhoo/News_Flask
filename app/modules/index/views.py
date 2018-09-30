406a3131a963f6abd83801a3144c5942000e5097
eric <278810505@qq.com>
2018-09-30 15:58:05 +0800

    1.首页右上角内容逻辑显示

--- app/modules/index/views.py
+++ app/modules/index/views.py
@@ -1,12 +1,21 @@
+from app.models import User
 from . import index_bp
-from flask import render_template, current_app
+from flask import render_template, current_app, session
 
 
 @index_bp.route('/')
 def hello_world():
-    # current_app.logger.debug("debug")
-    # current_app.logger.error("error")
-    return render_template('news/index.html')
+    # 获取到当前登录用户的id
+    user_id = session.get("user_id")
+    # 通过id获取用户信息
+    user = None  # type=User
+    if user_id:
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
+
+    return render_template("news/index.html", data={"user_info": user.to_dict() if user else None})
 
 
 @index_bp.route('/favicon.ico')
