b1f6e38b6e0adcc3c5a2da83af2ef21473dfcae2
eric <278810505@qq.com>
2018-10-08 16:18:52 +0800

    1.完成新闻分类展示功能 2.完成新闻列表数据分页加载功能

--- app/modules/index/views.py
+++ app/modules/index/views.py
@@ -1,11 +1,12 @@
 from app import constants
-from app.models import User, News
+from app.models import User, News, Category
+from app.utils.response_code import RET
 from . import index_bp
-from flask import render_template, current_app, session
+from flask import render_template, current_app, session, request, jsonify
 
 
 @index_bp.route('/')
-def hello_world():
+def index():
     # 获取到当前登录用户的id
     user_id = session.get("user_id")
     # 通过id获取用户信息
@@ -27,9 +28,19 @@ def hello_world():
     for news in new_list if new_list else []:
         click_news_list.append(news.to_basic_dict())
 
+    # 获取新闻分类数据
+    categories = Category.query.all()
+    # 定义列表保存分类数据
+    categories_dicts = []
+    for category in categories:
+        # 拼接内容
+        # print(category)
+        categories_dicts.append(category.to_dict())
+
     data = {
         'user_info': user.to_dict() if user else None,
-        'click_news_list': click_news_list
+        'click_news_list': click_news_list,
+        'categories': categories_dicts
     }
 
     return render_template("news/index.html", data=data)
@@ -38,3 +49,58 @@ def hello_world():
 @index_bp.route('/favicon.ico')
 def favicon():
     return current_app.send_static_file("news/favicon.ico")
+
+
+@index_bp.route('/newslist')
+def get_news_list():
+    """
+    获取指定分类的新闻列表
+    1.获取参数
+    2.校验参数
+    3.查询数据
+    4.返回数据
+    :return:
+    """
+    # 1.获取参数
+    args_dict = request.args
+    page = args_dict.get("page", "1")
+    per_page = args_dict.get("per_page", constants.HOME_PAGE_MAX_NEWS)
+    category_id = args_dict.get("cid", "1")
+
+    # 2.校验参数
+    try:
+        page = int(page)
+        per_page = int(per_page)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    # 3.查询数据并分页
+    filters = []
+    # 如果不分类id不为1,那么添加分类id的过滤
+    if category_id != "1":
+        filters.append(News.category_id == category_id)
+
+    try:
+        paginate = News.query.filter(*filters).order_by(News.create_time.desc()).paginate(page, per_page, False)
+        # 获取查询出来的数据
+        items = paginate.items
+        # 获取到总页数
+        total_page = paginate.pages
+        # 获取到当前页数
+        current_page = paginate.page
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据查询失败")
+
+    news_lst = []
+    for news in items:
+        news_lst.append(news.to_dict())
+
+    data = {
+        "news_list": news_lst,
+        "current_page": current_page,
+        "total_page": total_page
+    }
+
+    return jsonify(errno=RET.OK, errmsg="查询新闻列表数据成功", data=data )
--- app/static/news/js/index.js
+++ app/static/news/js/index.js
@@ -1,28 +1,31 @@
 var currentCid = 1; // 当前分类 id
 var cur_page = 1; // 当前页
 var total_page = 1;  // 总页数
-var data_querying = true;   // 是否正在向后台获取数据
+var data_querying = false;   // 是否正在向后台获取数据
 
 
 $(function () {
+    // 首次进入,去加载新闻列表数据
+    updateNewsData();
     // 首页分类切换
     $('.menu li').click(function () {
-        var clickCid = $(this).attr('data-cid')
+        var clickCid = $(this).attr('data-cid');
         $('.menu li').each(function () {
             $(this).removeClass('active')
-        })
-        $(this).addClass('active')
+        });
+        $(this).addClass('active');
 
         if (clickCid != currentCid) {
             // 记录当前分类id
-            currentCid = clickCid
+            currentCid = clickCid;
 
             // 重置分页参数
-            cur_page = 1
-            total_page = 1
+            cur_page = 1;
+            total_page = 1;
+            data_querying = false
             updateNewsData()
         }
-    })
+    });
 
     //页面滚动加载相关
     $(window).scroll(function () {
@@ -41,10 +44,58 @@ $(function () {
 
         if ((canScrollHeight - nowScroll) < 100) {
             // TODO 判断页数，去更新新闻数据
+            if(!data_querying){
+                // 正在查询数据设置标志位true
+                data_querying = true;
+                // 如果当前页数还没到达总页数,则进行查询
+                if(cur_page < total_page){
+                    // 向后端发送请求
+                    updateNewsData();
+                }else{
+                    data_querying = false;
+                }
+
+            }
+
         }
     })
 })
 
 function updateNewsData() {
     // TODO 更新新闻数据
+    var params = {
+        "page": cur_page,
+        "cid": currentCid,
+        "per_page": 10
+    }
+
+    $.get("/newslist", params, function (resp) {
+        //数据返回,则把查询标识为置为false,可下次继续查询
+        data_querying = false
+        if(resp){
+            total_page = resp.data.total_page;
+            if (cur_page == 1){
+                // 先清空原数据
+                $(".list_con").html('');
+            }
+
+            cur_page += 1;
+
+            // 显示数据
+            for(var i=0; i<resp.data.news_list.length; i++){
+                var news = resp.data.news_list[i];
+                var content = '<li>';
+                content += '<a href="#" class="news_pic fl"><img src="' + news.index_image_url + '?imageView2/1/w/170/h/170"></a>'
+                content += '<a href="#" class="news_title fl">' + news.title + '</a>'
+                content += '<a href="#" class="news_detail fl">' + news.digest + '</a>'
+                content += '<div class="author_info fl">'
+                content += '<div class="source fl">来源：' + news.source + '</div>'
+                content += '<div class="time fl">' + news.create_time + '</div>'
+                content += '</div>';
+                content += '</li>';
+                $(".list_con").append(content)
+            }
+        }
+
+    })
 }
