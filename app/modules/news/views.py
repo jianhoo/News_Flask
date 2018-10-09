2dca048e67a9d980f784adda61167b2691a5cf7f
eric <278810505@qq.com>
2018-10-09 16:19:23 +0800

    1.完成评论新闻功能 2.完成刷新新闻评论列表功能

--- app/modules/news/views.py
+++ app/modules/news/views.py
@@ -1,5 +1,5 @@
 from app import constants, db
-from app.models import News
+from app.models import News, Comment
 from app.utils.common import user_login_data
 from app.utils.response_code import RET
 from . import news_bp
@@ -9,7 +9,6 @@ from flask import render_template, current_app, abort, g, request, jsonify
 @news_bp.route("/<int:news_id>")
 @user_login_data
 def news_detail(news_id):
-
     try:
         cur_news = News.query.get(news_id)
     except Exception as e:
@@ -20,7 +19,7 @@ def news_detail(news_id):
         # 返回数据未找到的页面
         abort(404)
 
-        cur_news.clicks += 1
+    cur_news.clicks += 1
 
     # 获取点击排行数据
     new_list = None
@@ -40,11 +39,25 @@ def news_detail(news_id):
         if cur_news in g.user.collection_news:
             is_collected = True
 
+    # 获取当前新闻评论
+    comments = []
+    try:
+        comments = (Comment.query.filter(Comment.news_id == news_id)) \
+            .order_by(Comment.create_time.desc()).all()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="评论查询失败")
+
+    comment_list = []
+    for item in comments:
+        comment_list.append(item.to_dict())
+
     data = {
         "news": cur_news.to_dict(),
         "user_info": g.user.to_dict() if g.user else None,
         "click_news_list": click_news_list,
-        "is_collected": is_collected
+        "is_collected": is_collected,
+        "comments": comment_list
     }
     return render_template('news/detail.html', data=data)
 
@@ -91,3 +104,48 @@ def news_collect():
         return jsonify(errno=RET.DBERR, errmsg="数据保存失败")
 
     return jsonify(errno=RET.OK, errmsg="操作成功")
+
+
+@news_bp.route("/news_comment", methods=['POST'])
+@user_login_data
+def add_news_comment():
+    """添加评论"""
+    user = g.user
+    if not user:
+        return jsonify(errno=RET.SESSIONERR, errmsg="用户未登陆")
+    # 获取参数
+    data_dict = request.json
+    news_id = data_dict.get("news_id")
+    comment_str = data_dict.get("comment")
+    parent_id = data_dict.get("parent_id")
+
+    if not all([news_id, comment_str]):
+        jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据查询失败")
+
+    if not news:
+        return jsonify(errno=RET.NODATA, errmsg="该新闻不存在")
+
+    # 初始化模型,保存数据
+    comment = Comment()
+    comment.user_id = user.id
+    comment.news_id = news_id
+    comment.content = comment_str
+
+    if parent_id:
+        comment.parent_id = parent_id
+
+    try:
+        db.session.add(comment)
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="保存评论数据失败")
+
+    # 返回响应
+    return jsonify(errno=RET.OK, errmsg="评论成功", data=comment.to_dict())
--- app/static/news/js/detail.js
+++ app/static/news/js/detail.js
@@ -4,7 +4,7 @@ function getCookie(name) {
 }
 
 
-$(function(){
+$(function () {
 
     // 打开登录框
     $('.comment_form_logout').click(function () {
@@ -16,32 +16,32 @@ $(function(){
         var news_id = $(".collection").attr("data-newid")
         var action = "collect"
         var params = {
-            "news_id":news_id,
-            "action":action
+            "news_id": news_id,
+            "action": action
         }
         $.ajax({
-            url:"/news/news_collect",
-            type:"post",
-            contentType:"application/json",
-            headers:{
+            url: "/news/news_collect",
+            type: "post",
+            contentType: "application/json",
+            headers: {
                 "X-CSRFToken": getCookie("csrf_token")
             },
             data: JSON.stringify(params),
             success: function (resp) {
-                if(resp.errno == "0"){
+                if (resp.errno == "0") {
                     // 收藏成功
                     // 隐藏收藏成功
                     $(".collection").hide();
                     // 显示取消收藏按钮
                     $(".collected").show();
-                }else if(resp.errno == "4101"){
+                } else if (resp.errno == "4101") {
                     $(".login_form_con").show()
-                }else{
+                } else {
                     alert(resp.errmsg)
                 }
             }
         })
-       
+
     });
 
     // 取消收藏
@@ -53,70 +53,125 @@ $(function(){
             "action": action
         };
         $.ajax({
-            url:"/news/news_collect",
-            type:"post",
-            contentType:"application/json",
-            headers:{
+            url: "/news/news_collect",
+            type: "post",
+            contentType: "application/json",
+            headers: {
                 "X-CSRFToken": getCookie("csrf_token")
             },
             data: JSON.stringify(params),
             success: function (resp) {
-                if(resp.errno == "0"){
+                if (resp.errno == "0") {
                     // 收藏成功
                     // 显示收藏按钮
                     $(".collection").show();
                     // 隐藏取消收藏按钮
                     $(".collected").hide();
-                }else if(resp.errno == "4101"){
+                } else if (resp.errno == "4101") {
                     $(".login_form_con").show();
-                }else{
+                } else {
                     alert(resp.errmsg);
                 }
 
             }
         })
-     
+
     });
 
-        // 评论提交
+    // 评论提交
     $(".comment_form").submit(function (e) {
         e.preventDefault();
+        var news_id = $(this).attr('data-newsid');
+        var news_comment = $('.comment_input').val();
 
+        if (!news_comment) {
+            alert("请输入评论内容");
+            return
+        }
+
+        var params = {
+            "news_id": news_id,
+            "comment": news_comment
+        };
+
+        $.ajax({
+            url: "/news/news_comment",
+            type: "post",
+            contentType: "application/json",
+            headers: {
+                "X-CSRFToken": getCookie("csrf_token")
+            },
+            data: JSON.stringify(params),
+            success: function (resp) {
+                if (resp.errno == "0") {
+                    var comment = resp.data
+                    // 拼接内容
+                    var comment_html = ''
+                    comment_html += '<div class="comment_list">'
+                    comment_html += '<div class="person_pic fl">'
+                    if (comment.user.avatar_url) {
+                        comment_html += '<img src="' + comment.user.avatar_url + '" alt="用户图标">'
+                    } else {
+                        comment_html += '<img src="../../static/news/images/person01.png" alt="用户图标">'
+                    }
+                    comment_html += '</div>'
+                    comment_html += '<div class="user_name fl">' + comment.user.nick_name + '</div>'
+                    comment_html += '<div class="comment_text fl">'
+                    comment_html += comment.content
+                    comment_html += '</div>'
+                    comment_html += '<div class="comment_time fl">' + comment.create_time + '</div>'
+
+                    comment_html += '<a href="javascript:;" class="comment_up fr" data-commentid="' + comment.id + '" data-newsid="' + comment.news_id + '">赞</a>'
+                    comment_html += '<a href="javascript:;" class="comment_reply fr">回复</a>'
+                    comment_html += '<form class="reply_form fl" data-commentid="' + comment.id + '" data-newsid="' + news_id + '">'
+                    comment_html += '<textarea class="reply_input"></textarea>'
+                    comment_html += '<input type="button" value="回复" class="reply_sub fr">'
+                    comment_html += '<input type="reset" name="" value="取消" class="reply_cancel fr">'
+                    comment_html += '</form>'
+
+                    comment_html += '</div>'
+
+                    //拼接到内容的前面
+                    $(".comment_list_con").prepend(comment_html)
+                    //让comment_sub失去焦点
+                    $(".comment_sub").blur();
+                    //清空输入框内容
+                    $(".comment_input").val("")
+                }else{
+                    alert(resp.errmsg)
+                }
+            }
+        })
     })
 
-    $('.comment_list_con').delegate('a,input','click',function(){
+    $('.comment_list_con').delegate('a,input', 'click', function () {
 
         var sHandler = $(this).prop('class');
 
-        if(sHandler.indexOf('comment_reply')>=0)
-        {
+        if (sHandler.indexOf('comment_reply') >= 0) {
             $(this).next().toggle();
         }
 
-        if(sHandler.indexOf('reply_cancel')>=0)
-        {
+        if (sHandler.indexOf('reply_cancel') >= 0) {
             $(this).parent().toggle();
         }
 
-        if(sHandler.indexOf('comment_up')>=0)
-        {
+        if (sHandler.indexOf('comment_up') >= 0) {
             var $this = $(this);
-            if(sHandler.indexOf('has_comment_up')>=0)
-            {
+            if (sHandler.indexOf('has_comment_up') >= 0) {
                 // 如果当前该评论已经是点赞状态，再次点击会进行到此代码块内，代表要取消点赞
                 $this.removeClass('has_comment_up')
-            }else {
+            } else {
                 $this.addClass('has_comment_up')
             }
         }
 
-        if(sHandler.indexOf('reply_sub')>=0)
-        {
+        if (sHandler.indexOf('reply_sub') >= 0) {
             alert('回复评论')
         }
     })
 
-        // 关注当前新闻作者
+    // 关注当前新闻作者
     $(".focus").click(function () {
 
     })
