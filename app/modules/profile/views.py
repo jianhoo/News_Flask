4f7abb3287ec150ecb33b3846760fe24f43ce428
eric <278810505@qq.com>
2018-10-10 15:32:07 +0800

    1.新增修改密码功能

--- app/modules/profile/views.py
+++ app/modules/profile/views.py
@@ -105,3 +105,35 @@ def pic_info():
     # 4.返回上传的结果<avatar_url>
     return jsonify(errno=RET.OK, errmsg="图片保存成功",
                    data={"avatar_url": constants.QINIU_DOMIN_PREFIX + url})
+
+
+@profile_bp.route("/pass_info", methods=["GET", "POST"])
+@user_login_data
+def pass_info():
+    if request.method == "GET":
+        return render_template("profile/user_pass_info.html")
+
+    # 1.获取到传入参数
+    data_dict = request.json
+    old_password = data_dict.get("old_password")
+    new_password = data_dict.get("new_password")
+
+    if not all([old_password, new_password]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数有误")
+
+    # 2.获取当前登录用户的信息
+    user = g.user
+
+    if not user.check_passowrd(old_password):
+        return jsonify(errno=RET.PWDERR, errmsg="原密码有误")
+
+    # 更新数据
+    user.password = new_password
+    try:
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        return jsonify(errno=RET.DBERR, errmsg="保存数据失败")
+
+    return jsonify(errno=RET.OK, errmsg="保存成功")
--- app/static/news/js/user_pass_info.js
+++ app/static/news/js/user_pass_info.js
@@ -9,5 +9,38 @@ $(function () {
         e.preventDefault();
 
         // TODO 修改密码
+        var params = {};
+        $(this).serializeArray().map(function (x) {
+            params[x.name] = x.value;
+        });
+
+        // 取到两次密码进行判断
+        var new_password = params["new_password"];
+        var confirm_password = params["confirm_password"];
+
+        if (new_password != confirm_password) {
+            alert("两次密码输入不一致")
+            return
+        }
+
+        $.ajax({
+            url: "/profile/pass_info",
+            type: "POST",
+            contentType: "application/json",
+            headers: {
+                "X-CSRFToken": getCookie("csrf_token")
+            },
+            data: JSON.stringify(params),
+            success: function (resp) {
+                if (resp.errno == "0") {
+                    // 修改成功
+                    alert("修改成功");
+                    indow.location.reload()
+                } else {
+                    alert(resp.errmsg)
+                }
+            }
+        })
+
     })
-})
+});
